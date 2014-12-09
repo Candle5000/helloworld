f445fac6ea848ef859f848fa0155de02e694fd85
Candle5000 <candle5000@icloud.com>
2014-12-10 00:53:18 +0900

    feature commit test 2

--- helloworld.c
+++ helloworld.c
@@ -2,7 +2,11 @@
 
 int main(void) {
 	for(int i = 0; i < 10; i++) {
-		printf("HelloWorld\n");
+		if(i % 3 == 0) {
+			printf("%d\n", i);
+		} else {
+			printf("HelloWorld\n");
+		}
 	}
 	return(0);
 }
